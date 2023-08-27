e4eb1d71aacb3a3be9e51eca7c88f4c4e2c2b6ae
mehedi-h <mehedihs2015@gmail.com>
2023-08-27 16:39:31 +0600

    modified

--- js/main.js
+++ js/main.js
@@ -5,8 +5,18 @@ function loadPosts (){
 }
 
 function postsData (posts) {
+    const postContainer = document.getElementById('post-container')
+
     for (const post of posts) {
-        console.log(post)
+        const div = document.createElement('div');
+        div.classList.add('post')
+
+        div.innerHTML = `
+            <h4>User : ${post.userId}</h4>
+            <h5>Post : ${post.title}</h5>
+            <p>Post Description : ${post.body}</p>
+        `;
+        postContainer.appendChild(div)
     }
 }
 
